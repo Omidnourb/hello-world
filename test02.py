8398a508a74422f73455efdb25c4a6471b9a7424
Omidnourb <Omidnourb@users.noreply.com>
2016-08-26 17:11:12 -0400

    introduce third test

---
+++ test02.py
@@ -0,0 +1,21 @@
+#practice
+a=False
+if a==True:
+    print ('True')
+else:
+    print ('False')
+
+b=False
+
+if a==b:
+ print ('True')
+else:
+ print ('False')
+
+if a!= b:
+ print('True')
+else:
+ print('False')
+
+c=a+b;  days = ['Monday', 'Tuesday', 'Wednesday',
+         'Thursday', 'Friday']; print(days)
